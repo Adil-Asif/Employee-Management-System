fcef865a1ad0467e05d4b47e0e4e00d7ac4aa84e
Adil Asif <adilasif680@gmai.com>
2022-05-23 06:55:34 +0500

    Registration Form Integration

--- server/app/routes/signup.js
+++ server/app/routes/signup.js
@@ -1,62 +1,67 @@
-var express = require('express')
-var app = express()
-router = express.Router()
-app.use('/', router)
-const bcrypt = require('bcrypt')
-var db = require('../models/database')
-var session = require('sessionstorage')
+var express = require("express");
+var app = express();
+router = express.Router();
+const uuid = require("uuid");
+app.use("/", router);
+const bcrypt = require("bcrypt");
+var db = require("../models/database");
+var session = require("sessionstorage");
 
-router.post('/', (req, res) => {
-    if (req.body.signup) {
-        console.log(req.body)
-        var sql = 'insert into employee values(?,?,?,?,?,?,?)'
-        db.query(sql, [0, req.body.signup.username, req.body.signup.emailaddress, bcrypt.hashSync(req.body.signup.password, 5),'', '', false],
-            (err, result) => {
-                if (err) {
-                    console.log('error', err)
-                    if (err.includes('emailaddress')) {
-                        session.setItem('signup', false)
-                        res.send({ loggedIn: false, error: 'Email already exists' })
-                    }
-                    else if (err.includes('phone')) {
-                        session.setItem('signup', false)
-                        res.send({ loggedIn: false, error: 'Phone number already exists' })
-                    }
-                }
-                else if (result) {
-                    console.log('result', result)
-                    session.setItem('userid', result.insertId)
-                    session.setItem('completestage', false)
-                    const i1 = session.getItem('userid')
-                    console.log('id', i1)
-                    res.send({ loggedIn: true, userid: result.insertId, error: null })
-                }
-            })
-    }
-    else if (req.body.login) {
-        console.log(req.body)
-        var check = false
-        var sql = "select *from employee where emailaddress = ?"
-        db.query(sql, [
-            req.body.login.emailaddress
-        ], (err, result) => {
-            if (result.length > 0) {
-                check = bcrypt.compareSync(
-                    req.body.login.password,
-                    result[0].password,
-                    (err, res) => {
-                        if (err)
-                            res.send({ loggedIn: false })
-                    })
-                    if(check){
-                        res.send({loggedIn:true,userid:result[0].employee_id})
-                    }
-                    else
-                    {
-                        res.send({loggedIn:false})
-                    }
+router.post("/", (req, res) => {
+  //   console.log(req.body, "output");
+  if (req.body.signup) {
+    console.log(req.body);
+    var sql = "insert into user_details values(?,?,?,?,?,?,?)";
+    const userID = uuid.v1();
+    db.query(
+      sql,
+      [
+        userID,
+        req.body.signup.username,
+        req.body.signup.emailaddress,
+        bcrypt.hashSync(req.body.signup.password, 5),
+        "",
+        "",
+        false,
+      ],
+      (err, result) => {
+        if (err) {
+          console.log("error", err);
+          res.send("Email already registered");
+        } else if (result) {
+          console.log("result", result);
+          session.setItem("userid", userID);
+          session.setItem("completestage", false);
+          const i1 = session.getItem("userid");
+          console.log("id", i1, "h1");
+          res.send({ userid: userID });
         }
-        })
-    }
-})
+      }
+    );
+  } else if (req.body.login) {
+    // console.log(req.body);
+    var check = false;
+    var sql = "select *from user_details where emailaddress = ?";
+    db.query(sql, [req.body.login.emailaddress], (err, result) => {
+      if (err) {
+        res.send("Invalid email or password");
+      } else {
+        if (result.length > 0) {
+          check = bcrypt.compareSync(
+            req.body.login.password,
+            result[0].password,
+            (err, res) => {
+              if (err) res.send({ loggedIn: false });
+            }
+          );
+          if (check) {
+            res.send({ userid: result[0].userId });
+          } else {
+            res.send({ loggedIn: false });
+          }
+        }
+      }
+    });
+  }
+});
 module.exports = router;
